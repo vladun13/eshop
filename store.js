de1d984b0b23a2f01f04e3f665a6fdcddcbad759
vladun13 <vladun@ukr.net>
2018-07-28 01:05:31 +0300

    shop

--- store.js
+++ store.js
@@ -8,9 +8,9 @@ var storeGenerator = function () {
 				// for (var i = 0; i < orderedProducts.length; i++) {
 				// 	print += orderedProducts[i].name + ' ';
 				// }
-				print = orderedProducts.length;
+				print =  "מוצרים " + orderedProducts.length;
 			} else {
-				print = 'nothing'
+				print = '0 מוצרים'
 			}
 			$('#products').text(print);
 		},
@@ -27,23 +27,6 @@ var storeGenerator = function () {
 	}
 }
 var store = storeGenerator();
-// var productsData = [
-// 	{	"id": "product_1", "name" : "Schnitzel", "cost" : 19.9,	"description" : "Tasty schnitzel fingers", "image" : "img/1.jpg"},
-	
-// 	{	"id": "product_2",	"name" : "Rice sheets",	"cost" : 10.9,	"description" : "Rice sheets for sushi", "image" : "img/2.jpg"},
-	
-// 	{	"id": "product_3",	"name" : "Soap", "cost" : 9.9,	"description" : "Soap with apple flavor", "image" : "img/3.jpg"},
-	
-// 	{	"id": "product_4",  "name" : "Bran Flakes",	"cost" : 23.5, "description" : "Bran Flakes with vitamins",	"image" : "img/4.jpg"},
-	
-// 	{	"id": "product_5",	"name" : "Shower gel", "cost" : 10,	"description" : "Shower gel for men", "image" : "img/6.jpg"},
-	
-// 	{	"id": "product_6",	"name" : "Body lotion",	"cost" : 14.9, "description" : "Body lotion with vanilla flavor", "image" : "img/7.jpg"},
-
-// 	{	"id": "product_7",	"name" : "Cheese", "cost" : 12.9, "description" : "Cheese Adam Si", "image" : "img/8.jpg"},
-
-// 	{	"id": "product_8",	"name" : "Artik", "cost" : 7.9,	"description" : "Artik with banana and strawberry",	"image" : "img/10.jpg"}
-// ];
 var goodsContainer = $('#goods');
 $.get( "goods.json", function( productsData ) {
 	for (var i = 0; i < productsData.length; i++) {
